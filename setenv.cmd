0899faba9f3781910fe0a7cf9bb4b81e8d891029
Giovana Nogueira <ongiovana2016@gmail.com>
2024-09-09 14:04:57 -0300

    feat: setenv added

---
+++ setenv.cmd
@@ -0,0 +1,9 @@
+:: OpenDDS configure script: configure 
+set "ACE_ROOT=C:\Users\ongio_1lak36v\Downloads\OpenDDS-3.29.1\ACE_wrappers"
+set "CIAO_ROOT=unused"
+set "DANCE_ROOT=unused"
+set "DDS_ROOT=C:\Users\ongio_1lak36v\Downloads\OpenDDS-3.29.1"
+set "MPC_ROOT=C:\Users\ongio_1lak36v\Downloads\OpenDDS-3.29.1\ACE_wrappers\MPC"
+set "PATH=%PATH%;C:\Users\ongio_1lak36v\Downloads\OpenDDS-3.29.1\ACE_wrappers\bin;C:\Users\ongio_1lak36v\Downloads\OpenDDS-3.29.1\bin;C:\Users\ongio_1lak36v\Downloads\OpenDDS-3.29.1\ACE_wrappers\lib;C:\Users\ongio_1lak36v\Downloads\OpenDDS-3.29.1\lib"
+set "RAPIDJSON_ROOT=C:\Users\ongio_1lak36v\Downloads\OpenDDS-3.29.1\tools\rapidjson"
+set "TAO_ROOT=C:\Users\ongio_1lak36v\Downloads\OpenDDS-3.29.1\ACE_wrappers\TAO"
