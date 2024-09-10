b55ad289f9528fe954dfe1ce694b837bdeb12e57
Giovana Nogueira <ongiovana2016@gmail.com>
2024-09-10 17:09:24 -0300

    feat: skyportOperator publisher started for scenario 2

---
+++ scenario2/skyportOperatorDP/skyportOperatorPub.cpp
@@ -0,0 +1,62 @@
+#ifdef ACE_AS_STATIC_LIBS
+#include <C:/Users/ongio_1lak36v/Downloads/OpenDDS-3.29.1/dds/DCPS/transport/tcp/Tcp.h>
+#endif
+#include <model/Sync.h>
+#include <ace/Log_Msg.h>
+#include "UATMTraits.h"
+
+#include <model/Sync.h>
+
+
+int ACE_TMAIN(int argc, ACE_TCHAR **argv)
+{
+  try
+  {
+    OpenDDS::Model::Application application(argc, argv);
+    UATM::uatmDCPS::DefaultUATMType model(application, argc, argv);
+
+    using OpenDDS::Model::UATM::uatmDCPS::Elements;
+
+    DDS::DataWriter_var writer = model.writer(Elements::DataWriters::flightRoutesDW_SKO);
+
+    UATM::flightRoutesInfoDataWriter_var writer_var = UATM::flightRoutesInfoDataWriter::_narrow(writer.in());
+
+    if (CORBA::is_nil(writer_var.in())) {
+        ACE_ERROR_RETURN((LM_ERROR,
+                          ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
+                          ACE_TEXT(" _narrow failed!\n")),
+                         -1);
+    }
+
+    OpenDDS::Model::WriterSync ws(writer);
+    {
+      UATM::flightRoutesInfo fr;
+
+      // Populate message and send
+      fr.flight_route_id = 23;
+      fr.waypoints = ["a", "b"];
+      fr.flight_id = 7;
+      fr.estimated_time = "32323";
+      fr.status = true;
+
+      DDS::ReturnCode_t error = writer_var->write(fr, DDS::HANDLE_NIL);
+
+      if (error != DDS::RETCODE_OK) {
+          ACE_ERROR((LM_ERROR,
+                     ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
+                     ACE_TEXT(" wribte returned %d!\n"), error));
+      }
+    }
+  } catch (const CORBA::Exception& e) {
+    e._tao_print_exception("Exception caught in main():");
+    return -1;
+
+  } catch( const std::exception& ex) {
+    ACE_ERROR_RETURN((LM_ERROR,
+                      ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
+                      ACE_TEXT(" Exception caught: %C\n"),
+                      ex.what()),
+                     -1);
+  }
+  return 0;
+}
