1ef79b81eeb4f209b05bd7666e31fd96278089f4
Giovana Nogueira <ongiovana2016@gmail.com>
2024-09-10 20:08:51 -0300

    feat: tolPadManager publisher started for scenario 4

---
+++ scenario4/tolPadManagerDP/tolPadManagerPub.cpp
@@ -0,0 +1,60 @@
+#ifdef ACE_AS_STATIC_LIBS
+#include <C:/Users/ongio_1lak36v/Downloads/OpenDDS-3.29.1/dds/DCPS/transport/tcp/Tcp.h>
+#endif
+#include <model/Sync.h>
+#include <ace/Log_Msg.h>
+#include "UATMTraits.h"
+
+#include <model/Sync.h>
+
+
+int ACE_TMAIN(int argc, ACE_TCHAR **argv)
+{
+  try
+  {
+    OpenDDS::Model::Application application(argc, argv);
+    UATM::uatmDCPS::DefaultUATMType model(application, argc, argv);
+
+    using OpenDDS::Model::UATM::uatmDCPS::Elements;
+
+    DDS::DataWriter_var writer_availability = model.writer(Elements::DataWriters::tolPadAvailabilityDW_TP);
+    UATM::availabilityInfoDataWriter_var writer_availability_var = UATM::availabilityInfoDataWriter::_narrow(writer_availability.in());
+
+    if (CORBA::is_nil(writer_availability_var.in())) {
+        ACE_ERROR_RETURN((LM_ERROR,
+                          ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
+                          ACE_TEXT(" _narrow failed!\n")),
+                         -1);
+    }
+
+    OpenDDS::Model::WriterSync ws(writer_availability);
+    {
+      UATM::availabilityInfo ai;
+
+      // Populate message and send
+      ai.request_id = 23;
+      ai.resource_type = "22";
+      ai.status = true;
+      ai.location = "sa";
+      ai.availability_time = "true";
+
+      DDS::ReturnCode_t error = writer_availability_var->write(ai, DDS::HANDLE_NIL);
+
+      if (error != DDS::RETCODE_OK) {
+          ACE_ERROR((LM_ERROR,
+                     ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
+                     ACE_TEXT(" write returned %d!\n"), error));
+      }
+  } catch (const CORBA::Exception& e) {
+    e._tao_print_exception("Exception caught in main():");
+    return -1;
+
+  } catch( const std::exception& ex) {
+    ACE_ERROR_RETURN((LM_ERROR,
+                      ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
+                      ACE_TEXT(" Exception caught: %C\n"),
+                      ex.what()),
+                     -1);
+  }
+  return 0;
+}
