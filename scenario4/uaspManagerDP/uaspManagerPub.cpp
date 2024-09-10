558b2cb379f6fc8e96ee9f519839545fd69240d6
Giovana Nogueira <ongiovana2016@gmail.com>
2024-09-10 20:13:11 -0300

    feat: uaspManager publisher started for scenario 4

---
+++ scenario4/uaspManagerDP/uaspManagerPub.cpp
@@ -0,0 +1,59 @@
+#ifdef ACE_AS_STATIC_LIBS
+#include <C:/Users/ongio_1lak36v/Downloads/OpenDDS-3.29.1/dds/DCPS/transport/tcp/Tcp.h>
+#endif
+#include <model/Sync.h>
+#include <ace/Log_Msg.h>
+#include "UATMTraits.h"
+
+#include <model/Sync.h>
+
+
+int ACE_TMAIN(int argc, ACE_TCHAR **argv)
+{
+  try
+  {
+    OpenDDS::Model::Application application(argc, argv);
+    UATM::uatmDCPS::DefaultUATMType model(application, argc, argv);
+
+    using OpenDDS::Model::UATM::uatmDCPS::Elements;
+
+    DDS::DataWriter_var writer_assign = model.writer(Elements::DataWriters::changeRecDW_UASP);
+    UATM::tolPadRequestDataWriter_var writer_assign_var = UATM::tolPadRequestDataWriter::_narrow(writer_assign.in());
+
+    if (CORBA::is_nil(writer_assign_var.in())) {
+        ACE_ERROR_RETURN((LM_ERROR,
+                          ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
+                          ACE_TEXT(" _narrow failed!\n")),
+                         -1);
+    }
+
+    OpenDDS::Model::WriterSync ws(writer_assign);
+    {
+      UATM::tolPadRequest tr;
+
+      // Populate message and send
+      tr.assign_id = 23;
+      tr.flight_id = 22;
+      tr.tol_pad_id = 1;
+      tr.assign_time = "sa";
+
+      DDS::ReturnCode_t error = writer_assign_var->write(tr, DDS::HANDLE_NIL);
+
+      if (error != DDS::RETCODE_OK) {
+          ACE_ERROR((LM_ERROR,
+                     ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
+                     ACE_TEXT(" write returned %d!\n"), error));
+      }
+  } catch (const CORBA::Exception& e) {
+    e._tao_print_exception("Exception caught in main():");
+    return -1;
+
+  } catch( const std::exception& ex) {
+    ACE_ERROR_RETURN((LM_ERROR,
+                      ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
+                      ACE_TEXT(" Exception caught: %C\n"),
+                      ex.what()),
+                     -1);
+  }
+  return 0;
+}
