1c596a9395c5c69a4aecde169fd75066a7974c66
giovananog <giovana.nogueira@sou.unifal-mg.edu.br>
2024-09-21 20:23:21 -0300

    feat: update fleetOperatorDP for general scenario

---
+++ pre-departure scenario/general/fleetOperatorDP/Subscriber/ReaderListenerAvailability.h
@@ -0,0 +1,20 @@
+#ifndef READER_LISTENER_AVAILABILITY_H
+#define READER_LISTENER_AVAILABILITY_H
+
+#include <model/Sync.h>
+#include <ace/Log_Msg.h>
+#include <dds/DCPS/WaitSet.h>
+#include <vector>
+#include <tools/modeling/codegen/model/NullReaderListener.h>
+
+
+class ReaderListenerAvailability : public OpenDDS::Model::NullReaderListener {
+  public:
+    ReaderListenerAvailability(OpenDDS::Model::ReaderCondSync& rcs);
+    virtual void on_data_available(DDS::DataReader_ptr reader);
+  private:
+    OpenDDS::Model::ReaderCondSync& rcs_;
+    ACE_Thread_Mutex mutex_;
+};
+
+#endif // READER_LISTENER_AVAILABILITY_H
