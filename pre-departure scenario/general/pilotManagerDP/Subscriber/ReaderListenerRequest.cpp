b86d9a8ec3751697de39ab5eba44a3f2480d792e
giovananog <giovana.nogueira@sou.unifal-mg.edu.br>
2024-09-21 20:34:47 -0300

    feat: create pilotManagerDP for general scenario

---
+++ pre-departure scenario/general/pilotManagerDP/Subscriber/ReaderListenerRequest.cpp
@@ -0,0 +1,65 @@
+#include "../../model/UATMTraits.h"
+#include <tools/modeling/codegen/model/NullReaderListener.h>
+#include <model/Sync.h>
+#include <ace/Log_Msg.h>
+#include <dds/DCPS/WaitSet.h>
+#include "ReaderListenerRequest.h"
+
+ReaderListenerRequest::ReaderListenerRequest(OpenDDS::Model::ReaderCondSync &rcs)
+    : rcs_(rcs) {}
+
+void ReaderListenerRequest::on_data_available(DDS::DataReader_ptr reader)
+{
+  ACE_Guard<ACE_Thread_Mutex> g(mutex_);
+
+  UATM::flightAssignDataReader_var reader_i =
+      UATM::flightAssignDataReader::_narrow(reader);
+
+  if (CORBA::is_nil(reader_i.in()))
+  {
+    ACE_ERROR((LM_ERROR,
+               ACE_TEXT("ERROR: %N:%l: on_data_available() -")
+                   ACE_TEXT(" _narrow failed!\n")));
+    ACE_OS::exit(-1);
+  }
+
+  UATM::flightAssign msg;
+  DDS::SampleInfo info;
+
+  while (true)
+  {
+    std::cout << "\n\n"
+              << std::endl;
+    DDS::ReturnCode_t error = reader_i->take_next_sample(msg, info);
+    if (error == DDS::RETCODE_OK)
+    {
+      // // std::cout << "SampleInfo.sample_rank = " << info.sample_rank << std::endl;
+      if (info.valid_data)
+      {
+        std::cout << "----------------------------------" << std::endl
+                  << "        flightAssign:" << std::endl
+                  << "        -----------------" << std::endl
+                  << "Flight Assign ID: " << msg.flight_assign_id << std::endl
+                  << "Assign Time: " << msg.assign_time.in() << std::endl
+                  << "Operator ID: " << msg.operator_id << std::endl
+                  << "Assign Status: " << msg.assign_status << std::endl
+                  << "Resources ID: " << msg.resources_id << std::endl;
+      }
+      else
+      {
+        rcs_.signal();
+        break;
+      }
+    }
+    else
+    {
+      if (error != DDS::RETCODE_NO_DATA)
+      {
+        ACE_ERROR((LM_ERROR,
+                   ACE_TEXT("ERROR: %N:%l: on_data_available() -")
+                       ACE_TEXT(" take_next_sample failed!\n")));
+      }
+      break;
+    }
+  }
+}
