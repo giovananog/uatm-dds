d7d0250d92efd9d1960af09097d9cd82b98c968d
giovananog <giovana.nogueira@sou.unifal-mg.edu.br>
2024-09-21 20:36:39 -0300

    feat: create skyportManagerDP for general scenario

---
+++ pre-departure scenario/general/skyportManagerDP/Publisher/skyportManagerPub.cpp
@@ -0,0 +1,58 @@
+#ifdef ACE_AS_STATIC_LIBS
+#include <dds/DCPS/transport/tcp/Tcp.h>
+#endif
+#include <model/Sync.h>
+#include <ace/Log_Msg.h>
+#include "../../model/UATMTraits.h"
+
+int ACE_TMAIN(int argc, ACE_TCHAR **argv)
+{
+  try
+  {
+    OpenDDS::Model::Application application(argc, argv);
+    UATM::uatmDCPS::DefaultUATMType model(application, argc, argv);
+
+    using OpenDDS::Model::UATM::uatmDCPS::Elements;
+
+    DDS::DataWriter_var writer = model.writer(Elements::DataWriters::skyportAvailabilityDW_SKM);
+
+    UATM::availabilityInfoDataWriter_var writer_var = UATM::availabilityInfoDataWriter::_narrow(writer.in());
+
+    if (CORBA::is_nil(writer_var.in())) {
+        ACE_ERROR_RETURN((LM_ERROR,
+                          ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
+                          ACE_TEXT(" _narrow failed!\n")),
+                         -1);
+    }
+
+    OpenDDS::Model::WriterSync ws(writer);
+    {
+      UATM::availabilityInfo fa;
+
+      fa.resource_id = 1;
+      fa.resource_type = "resource_type";
+      fa.status = true;
+      fa.location = "location";
+      fa.availability_time = "availability_time";
+
+      DDS::ReturnCode_t error = writer_var->write(fa, DDS::HANDLE_NIL);
+
+      if (error != DDS::RETCODE_OK) {
+          ACE_ERROR((LM_ERROR,
+                     ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
+                     ACE_TEXT(" write returned %d!\n"), error));
+      }
+    }
+  } catch (const CORBA::Exception& e) {
+    e._tao_print_exception("Exception caught in main():");
+    return -1;
+
+  } catch( const std::exception& ex) {
+    ACE_ERROR_RETURN((LM_ERROR,
+                      ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
+                      ACE_TEXT(" Exception caught: %C\n"),
+                      ex.what()),
+                     -1);
+  }
+  return 0;
+}
