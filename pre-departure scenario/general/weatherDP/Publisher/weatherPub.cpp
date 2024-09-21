d6c433e9634c37d11256829619055ad54b1ee51d
giovananog <giovana.nogueira@sou.unifal-mg.edu.br>
2024-09-21 20:50:33 -0300

    feat: create weatherDP for general scenario

---
+++ pre-departure scenario/general/weatherDP/Publisher/weatherPub.cpp
@@ -0,0 +1,65 @@
+#ifdef ACE_AS_STATIC_LIBS
+#include <dds/DCPS/transport/tcp/Tcp.h>
+#endif
+#include <model/Sync.h>
+#include <ace/Log_Msg.h>
+#include "../../model/UATMTraits.h"
+
+int ACE_TMAIN(int argc, ACE_TCHAR **argv)
+{
+  try
+  {
+    OpenDDS::Model::Application application(argc, argv);
+    UATM::uatmDCPS::DefaultUATMType model3(application, argc, argv);
+
+    using OpenDDS::Model::UATM::uatmDCPS::Elements;
+
+    DDS::DataWriter_var writer = model3.writer(Elements::DataWriters::weatherInfoDW_WTR);
+
+    UATM::weatherInfoDataWriter_var writer_var = UATM::weatherInfoDataWriter::_narrow(writer.in());
+
+    if (CORBA::is_nil(writer_var.in()))
+    {
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
+                            ACE_TEXT(" _narrow failed!\n")),
+                       -1);
+    }
+
+    OpenDDS::Model::WriterSync ws(writer);
+    {
+      UATM::weatherInfo fr;
+
+      fr.weather_id = 1;
+      fr.location = "location";
+      fr.temperature = 2.3;
+      fr.wind_speed = 4.5;
+      fr.weather_condition = "weather_condition";
+      fr.observation_time = "observation_time";
+
+      DDS::ReturnCode_t error = writer_var->write(fr, DDS::HANDLE_NIL);
+
+      if (error != DDS::RETCODE_OK)
+      {
+        ACE_ERROR((LM_ERROR,
+                   ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
+                       ACE_TEXT(" wribte returned %d!\n"),
+                   error));
+      }
+    }
+  }
+  catch (const CORBA::Exception &e)
+  {
+    e._tao_print_exception("Exception caught in main():");
+    return -1;
+  }
+  catch (const std::exception &ex)
+  {
+    ACE_ERROR_RETURN((LM_ERROR,
+                      ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
+                          ACE_TEXT(" Exception caught: %C\n"),
+                      ex.what()),
+                     -1);
+  }
+  return 0;
+}
