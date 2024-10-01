8be0de1f10b487126f05250fe80e3486a3dc10aa
giovananog <giovana.nogueira@sou.unifal-mg.edu.br>
2024-09-30 22:38:13 -0300

    feat: update skyportManagerDP on general scenario

--- pre-departure scenario/general/skyportManagerDP/Publisher/skyportManagerPub.cpp
+++ pre-departure scenario/general/skyportManagerDP/Publisher/skyportManagerPub.cpp
@@ -1,9 +1,15 @@
-#ifdef ACE_AS_STATIC_LIBS
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <thread>
+#include <chrono>
 #include <dds/DCPS/transport/tcp/Tcp.h>
-#endif
-#include <model/Sync.h>
-#include <ace/Log_Msg.h>
 #include "../../model/UATMTraits.h"
+#include <model/Sync.h>
+#include "../utils/functions.h"
+
 
 int ACE_TMAIN(int argc, ACE_TCHAR **argv)
 {
@@ -18,39 +24,64 @@ int ACE_TMAIN(int argc, ACE_TCHAR **argv)
 
     UATM::availabilityInfoDataWriter_var writer_var = UATM::availabilityInfoDataWriter::_narrow(writer.in());
 
-    if (CORBA::is_nil(writer_var.in())) {
-        ACE_ERROR_RETURN((LM_ERROR,
-                          ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
-                          ACE_TEXT(" _narrow failed!\n")),
-                         -1);
+    if (CORBA::is_nil(writer_var.in()))
+    {
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
+                            ACE_TEXT(" _narrow failed!\n")),
+                       -1);
     }
 
     OpenDDS::Model::WriterSync ws(writer);
     {
-      UATM::availabilityInfo fa;
 
-      fa.resource_id = 1;
-      fa.resource_type = "resource_type";
-      fa.status = true;
-      fa.location = "location";
-      fa.availability_time = "availability_time";
+      while (true)
+      {
+        std::string filename = "skyportManagerDP/data/skyports.txt";
+        std::vector<Skyport> skyports = readSkyportsFromFile(filename);
 
-      DDS::ReturnCode_t error = writer_var->write(fa, DDS::HANDLE_NIL);
+        if (skyports.empty())
+        {
+          std::cout << "Todos os Skyports foram enviados!" << std::endl;
+          break;
+        }
 
-      if (error != DDS::RETCODE_OK) {
+        Skyport current_skyport = skyports.front();
+
+        UATM::availabilityInfo fa;
+
+        fa.resource_id = CORBA::string_dup(current_skyport.resource_id.c_str());
+        fa.resource_type = "skyport";
+        fa.available = current_skyport.available;
+        fa.skyport_id = CORBA::string_dup(current_skyport.resource_id.c_str());
+        fa.availability_time = "time";
+
+        DDS::ReturnCode_t error = writer_var->write(fa, DDS::HANDLE_NIL);
+
+        if (error != DDS::RETCODE_OK)
+        {
           ACE_ERROR((LM_ERROR,
                      ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
-                     ACE_TEXT(" write returned %d!\n"), error));
+                         ACE_TEXT(" write returned %d!\n"),
+                     error));
+        }
+
+        updateSkyportInFile(filename, current_skyport.resource_id);
+
+        std::this_thread::sleep_for(std::chrono::seconds(15));
       }
     }
-  } catch (const CORBA::Exception& e) {
+  }
+  catch (const CORBA::Exception &e)
+  {
     e._tao_print_exception("Exception caught in main():");
     return -1;
-
-  } catch( const std::exception& ex) {
+  }
+  catch (const std::exception &ex)
+  {
     ACE_ERROR_RETURN((LM_ERROR,
                       ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
-                      ACE_TEXT(" Exception caught: %C\n"),
+                          ACE_TEXT(" Exception caught: %C\n"),
                       ex.what()),
                      -1);
   }
