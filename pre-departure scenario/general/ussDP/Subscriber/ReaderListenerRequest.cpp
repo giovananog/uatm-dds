537b667e29cf611bab4f33cb19d492ab1220c1fa
giovananog <giovana.nogueira@sou.unifal-mg.edu.br>
2024-10-05 22:22:05 -0300

    feat: update ussDP on general scenario

--- pre-departure scenario/general/ussDP/Subscriber/ReaderListenerRequest.cpp
+++ pre-departure scenario/general/ussDP/Subscriber/ReaderListenerRequest.cpp
@@ -1,4 +1,4 @@
-  
+
 #include "../../model/UATMTraits.h"
 #include <tools/modeling/codegen/model/NullReaderListener.h>
 
@@ -8,48 +8,63 @@
 #include <dds/DCPS/WaitSet.h>
 #include "ReaderListenerRequest.h"
 
-ReaderListenerRequest::ReaderListenerRequest(OpenDDS::Model::ReaderCondSync& rcs)
-  : rcs_(rcs) {}
+ReaderListenerRequest::ReaderListenerRequest(OpenDDS::Model::ReaderCondSync &rcs)
+    : rcs_(rcs) {}
 
-void
-ReaderListenerRequest::on_data_available(DDS::DataReader_ptr reader) 
-  {
-    ACE_Guard<ACE_Thread_Mutex> g(mutex_);
+void ReaderListenerRequest::on_data_available(DDS::DataReader_ptr reader)
+{
 
-    UATM::acceptableRouteDataReader_var reader_i =
+  ACE_Guard<ACE_Thread_Mutex> g(mutex_);
+  static bool signal_sent = false;
+
+  UATM::acceptableRouteDataReader_var reader_i =
       UATM::acceptableRouteDataReader::_narrow(reader);
 
-    if (CORBA::is_nil(reader_i.in())) {
+  if (CORBA::is_nil(reader_i.in()))
+  {
     ACE_ERROR((LM_ERROR,
                ACE_TEXT("ERROR: %N:%l: on_data_available() -")
-               ACE_TEXT(" _narrow failed!\n")));
+                   ACE_TEXT(" _narrow failed!\n")));
     ACE_OS::exit(-1);
   }
 
-    UATM::acceptableRoute msg;
-    DDS::SampleInfo info;
+  UATM::acceptableRoute msg;
+  DDS::SampleInfo info;
 
-    while (true) {
-      DDS::ReturnCode_t error = reader_i->take_next_sample(msg, info);
-      if (error == DDS::RETCODE_OK) {
-        if (info.valid_data) {
-          std::cout << "| acceptableRoute: "
-                    << "acceptable_route_id:" << msg.acceptable_route_id.in() 
-                    << ",approved_by:" << msg.approved_by.in() 
-                    << ",estimated_time:" << msg.estimated_time.in() 
-                    << ",timestamp:" << msg.timestamp.in() << std::endl;
-        } else {
-            rcs_.signal();
-            break;
+  while (true)
+  {
+    DDS::ReturnCode_t error = reader_i->take_next_sample(msg, info);
+    if (error == DDS::RETCODE_OK)
+    {
+      if (info.valid_data)
+      {
+        std::cout << "| ansp acceptableRoute: "
+                  << "acceptable_route_id:" << msg.acceptable_route_id
+                  << ",approved_by:" << msg.approved_by.in()
+                  << ",estimated_time:" << msg.estimated_time.in()
+                  << ",flight_id:" << msg.flight_id.in()
+                  << ",timestamp:" << msg.timestamp.in() << std::endl;
+        break;
+      }
+      else
+      {
+        if (!signal_sent)
+        {
+          rcs_.signal();
+          signal_sent = true;
         }
-      } else {
-        if (error != DDS::RETCODE_NO_DATA) {
-        ACE_ERROR((LM_ERROR,
-                   ACE_TEXT("ERROR: %N:%l: on_data_available() -")
-                   ACE_TEXT(" take_next_sample failed!\n")));
-        }
-        // rcs_.signal();
         break;
       }
     }
-  };
+    else
+    {
+      if (error != DDS::RETCODE_NO_DATA)
+      {
+        ACE_ERROR((LM_ERROR,
+                   ACE_TEXT("ERROR: %N:%l: on_data_available() -")
+                       ACE_TEXT(" take_next_sample failed!\n")));
+      }
+      break;
+    }
+  }
+};
