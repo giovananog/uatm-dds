4bcbca635220f57f303efb5ae18964bb9cb48b0d
giovananog <giovana.nogueira@sou.unifal-mg.edu.br>
2024-10-16 21:02:00 -0300

    feat: update uaspManagersDP subscribers

--- pre-departure scenario/general/uaspManagerDP/Subscriber/ReaderListenerFlows.cpp
+++ pre-departure scenario/general/uaspManagerDP/Subscriber/ReaderListenerFlows.cpp
@@ -6,60 +6,72 @@
 #include <dds/DCPS/WaitSet.h>
 #include "ReaderListenerFlows.h"
 
+ReaderListenerFlows::ReaderListenerFlows(OpenDDS::Model::ReaderCondSync &rcs)
+    : rcs_(rcs) {}
 
-ReaderListenerFlows::ReaderListenerFlows(OpenDDS::Model::ReaderCondSync& rcs)
-  : rcs_(rcs) {}
+void ReaderListenerFlows::on_data_available(DDS::DataReader_ptr reader)
+{
+  ACE_Guard<ACE_Thread_Mutex> g(mutex_);
+  static bool signal_sent = false;
 
-void
-ReaderListenerFlows::on_data_available(DDS::DataReader_ptr reader) 
-  {
-    ACE_Guard<ACE_Thread_Mutex> g(mutex_);
-
-    UATM::trafficFlowsInfoDataReader_var reader_i =
+  UATM::trafficFlowsInfoDataReader_var reader_i =
       UATM::trafficFlowsInfoDataReader::_narrow(reader);
 
-    if (CORBA::is_nil(reader_i.in())) {
+  if (CORBA::is_nil(reader_i.in()))
+  {
     ACE_ERROR((LM_ERROR,
                ACE_TEXT("ERROR: %N:%l: on_data_available_request() -")
-               ACE_TEXT(" _narrow failed!\n")));
+                   ACE_TEXT(" _narrow failed!\n")));
     ACE_OS::exit(-1);
   }
 
-    UATM::trafficFlowsInfo msg;
-    DDS::SampleInfo info;
+  UATM::trafficFlowsInfo msg;
+  DDS::SampleInfo info;
 
-    while (true) {
-      DDS::ReturnCode_t error = reader_i->take_next_sample(msg, info);
-      if (error == DDS::RETCODE_OK) {
-        if (info.valid_data) {
-          std::cout << "| trafficFlowsInfo: " 
-                    << "flows_id:" << msg.flows_id 
-                    << ",area:" << msg.area.in() 
-                    << ",congestion_level:" << msg.congestion_level.in() 
-                    << ",affected_routes:" << msg.affected_routes.in() 
-                    << ",timestamp:" << msg.timestamp.in() << std::endl;
+  while (true)
+  {
+    DDS::ReturnCode_t error = reader_i->take_next_sample(msg, info);
+    if (error == DDS::RETCODE_OK)
+    {
+      if (info.valid_data)
+      {
+        std::cout << "| trafficFlowsInfo: "
+                  << "flows_id:" << msg.flows_id
+                  << ",area:" << msg.area.in()
+                  << ",congestion_level:" << msg.congestion_level.in()
+                  << ",affected_routes:" << msg.affected_routes.in()
+                  << ",timestamp:" << msg.timestamp.in() << std::endl;
 
-           std::ofstream outfile;
-                outfile.open("uaspManagerDP/data/flows.txt", std::ios_base::app);
-                
-                outfile << "flows_id:" << msg.flows_id << ","
-                        << "area:" << msg.area.in() << ","
-                        << "congestion_level:" << msg.congestion_level.in() << ","
-                        << "timestamp:" << msg.timestamp.in() << ","
-                        << "affected_routes:" << msg.affected_routes.in() << std::endl;
-                
-                outfile.close();
-        } else {
-            rcs_.signal();
-            break;
-        }
-      } else {
-        if (error != DDS::RETCODE_NO_DATA) {
-        ACE_ERROR((LM_ERROR,
-                   ACE_TEXT("ERROR: %N:%l: on_data_available_request() -")
-                   ACE_TEXT(" take_next_sample failed!\n")));
+        std::ofstream outfile;
+        outfile.open("uaspManagerDP/data/flows.txt", std::ios_base::app);
+
+        outfile << "flows_id:" << msg.flows_id << ","
+                << "area:" << msg.area.in() << ","
+                << "congestion_level:" << msg.congestion_level.in() << ","
+                << "timestamp:" << msg.timestamp.in() << ","
+                << "affected_routes:" << msg.affected_routes.in() << std::endl;
+
+        outfile.close();
+      }
+      else
+      {
+        if (!signal_sent)
+        {
+          rcs_.signal();
+          signal_sent = true;
         }
         break;
       }
     }
-  };
+    else
+    {
+      if (error != DDS::RETCODE_NO_DATA)
+      {
+        ACE_ERROR((LM_ERROR,
+                   ACE_TEXT("ERROR: %N:%l: on_data_available_request() -")
+                       ACE_TEXT(" take_next_sample failed!\n")));
+      }
+      break;
+    }
+  }
+};
--- pre-departure scenario/general/uaspManagerDP/Subscriber/ReaderListenerWeather.cpp
+++ pre-departure scenario/general/uaspManagerDP/Subscriber/ReaderListenerWeather.cpp
@@ -8,61 +8,74 @@
 #include <dds/DCPS/WaitSet.h>
 #include "ReaderListenerWeather.h"
 
-ReaderListenerWeather::ReaderListenerWeather(OpenDDS::Model::ReaderCondSync& rcs)
-  : rcs_(rcs) {}
+ReaderListenerWeather::ReaderListenerWeather(OpenDDS::Model::ReaderCondSync &rcs)
+    : rcs_(rcs) {}
 
-void
-ReaderListenerWeather::on_data_available(DDS::DataReader_ptr reader) 
-  {
-    ACE_Guard<ACE_Thread_Mutex> g(mutex_);
+void ReaderListenerWeather::on_data_available(DDS::DataReader_ptr reader)
+{
+  ACE_Guard<ACE_Thread_Mutex> g(mutex_);
+  static bool signal_sent = false;
 
-    UATM::weatherInfoDataReader_var reader_i =
+  UATM::weatherInfoDataReader_var reader_i =
       UATM::weatherInfoDataReader::_narrow(reader);
 
-    if (CORBA::is_nil(reader_i.in())) {
+  if (CORBA::is_nil(reader_i.in()))
+  {
     ACE_ERROR((LM_ERROR,
                ACE_TEXT("ERROR: %N:%l: on_data_available_request() -")
-               ACE_TEXT(" _narrow failed!\n")));
+                   ACE_TEXT(" _narrow failed!\n")));
     ACE_OS::exit(-1);
   }
 
-    UATM::weatherInfo msg;
-    DDS::SampleInfo info;
+  UATM::weatherInfo msg;
+  DDS::SampleInfo info;
 
-    while (true) {
-      DDS::ReturnCode_t error = reader_i->take_next_sample(msg, info);
-      if (error == DDS::RETCODE_OK) {
-        if (info.valid_data) {
-          std::cout << "| weatherInfo: " 
-                    << "weather_id:" << msg.weather_id 
-                    << ",location:" << msg.location.in() 
-                    << ",temperature:" << msg.temperature 
-                    << ",wind_speed:" << msg.wind_speed 
-                    << ",weather_condition:" << msg.weather_condition.in() 
-                    << ",observation_time:" << msg.observation_time.in() << std::endl;
+  while (true)
+  {
+    DDS::ReturnCode_t error = reader_i->take_next_sample(msg, info);
+    if (error == DDS::RETCODE_OK)
+    {
+      if (info.valid_data)
+      {
+        std::cout << "| weatherInfo: "
+                  << "weather_id:" << msg.weather_id
+                  << ",location:" << msg.location.in()
+                  << ",temperature:" << msg.temperature
+                  << ",wind_speed:" << msg.wind_speed
+                  << ",weather_condition:" << msg.weather_condition.in()
+                  << ",observation_time:" << msg.observation_time.in() << std::endl;
 
-          std::ofstream outfile;
-                outfile.open("uaspManagerDP/data/weather.txt", std::ios_base::app);
-                
-                outfile << "weather_id:" << msg.weather_id << ","
-                        << "location:" << msg.location.in() << ","
-                        << "temperature:" << msg.temperature << ","
-                        << "wind_speed:" << msg.wind_speed << ","
-                        << "weather_condition:" << msg.weather_condition.in() << ","
-                        << "observation_time:" << msg.observation_time.in() << std::endl;
-                
-                outfile.close();
-        } else {
-            rcs_.signal();
-            break;
-        }
-      } else {
-        if (error != DDS::RETCODE_NO_DATA) {
-        ACE_ERROR((LM_ERROR,
-                   ACE_TEXT("ERROR: %N:%l: on_data_available_request() -")
-                   ACE_TEXT(" take_next_sample failed!\n")));
+        std::ofstream outfile;
+        outfile.open("uaspManagerDP/data/weather.txt", std::ios_base::app);
+
+        outfile << "weather_id:" << msg.weather_id << ","
+                << "location:" << msg.location.in() << ","
+                << "temperature:" << msg.temperature << ","
+                << "wind_speed:" << msg.wind_speed << ","
+                << "weather_condition:" << msg.weather_condition.in() << ","
+                << "observation_time:" << msg.observation_time.in() << std::endl;
+
+        outfile.close();
+      }
+      else
+      {
+        if (!signal_sent)
+        {
+          rcs_.signal();
+          signal_sent = true;
         }
         break;
       }
     }
-  };
+    else
+    {
+      if (error != DDS::RETCODE_NO_DATA)
+      {
+        ACE_ERROR((LM_ERROR,
+                   ACE_TEXT("ERROR: %N:%l: on_data_available_request() -")
+                       ACE_TEXT(" take_next_sample failed!\n")));
+      }
+      break;
+    }
+  }
+};
