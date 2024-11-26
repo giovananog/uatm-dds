7627f1917f575834a5da8909e0d01f99b983e6e2
giovananog <giovana.nogueira@sou.unifal-mg.edu.br>
2024-11-26 09:18:05 -0300

    feat: update uaspManager publihser

--- pre-departure scenario/general/uaspManagerDP/Publisher/uaspManPub.cpp
+++ pre-departure scenario/general/uaspManagerDP/Publisher/uaspManPub.cpp
@@ -1,19 +1,19 @@
 #ifdef ACE_AS_STATIC_LIBS
-#include <dds/DCPS/transport/tcp/Tcp.h>  // Include the TCP transport layer if ACE is statically linked
+#include <dds/DCPS/transport/tcp/Tcp.h> // Include the TCP transport layer if ACE is statically linked
 #endif
 
-#include <iostream>        // Standard input/output stream
-#include <fstream>         // File stream operations
-#include <string>          // String manipulation
-#include <sstream>         // String stream
-#include <vector>          // Dynamic array
-#include <thread>          // Threading functionality
-#include <chrono>          // Time duration and timestamps
-#include <unordered_set>   // For storing unique elements efficiently
-#include <model/Sync.h>    // Custom synchronization model
-#include <ace/Log_Msg.h>   // ACE logging utility
-#include "../../model/UATMTraits.h"  // Includes specific traits for UATM (Urban Air Traffic Management)
-#include "../utils/functions.h"  // Utility functions
+#include <iostream>                 // Standard input/output stream
+#include <fstream>                  // File stream operations
+#include <string>                   // String manipulation
+#include <sstream>                  // String stream
+#include <vector>                   // Dynamic array
+#include <thread>                   // Threading functionality
+#include <chrono>                   // Time duration and timestamps
+#include <unordered_set>            // For storing unique elements efficiently
+#include <model/Sync.h>             // Custom synchronization model
+#include <ace/Log_Msg.h>            // ACE logging utility
+#include "../../model/UATMTraits.h" // Includes specific traits for UATM (Urban Air Traffic Management)
+#include "../utils/functions.h"     // Utility functions
 
 // Main entry point of the application
 int ACE_TMAIN(int argc, ACE_TCHAR **argv)
@@ -82,11 +82,14 @@ int ACE_TMAIN(int argc, ACE_TCHAR **argv)
     int acceptable_route_id = 1;
     int recommendation_id = 1;
     std::unordered_set<std::string> sent_flight_ids;
+    std::unordered_set<std::string> sent_flight_auth;
+    std::unordered_set<std::string> sent_flight_recs;
     std::unordered_set<std::string> sent_tolpads;
 
     // Record the start time for the operation
     auto startTime = std::chrono::steady_clock::now();
-    double duration = 100.0;  // Set a duration to monitor the process
+    double duration = 100.0; // Set a duration to monitor the process
+    int i = 0;
 
     // Main processing loop (runs indefinitely until duration is met)
     while (true)
@@ -118,8 +121,6 @@ int ACE_TMAIN(int argc, ACE_TCHAR **argv)
       // Synchronize the writer for tolpad assignments
       OpenDDS::Model::WriterSync ws(writer_assign);
       {
-        bool sent = false;
-
         // Loop through tolpad requests to check for unassigned ones
         for (const auto &tolpad : tolpads)
         {
@@ -147,7 +148,6 @@ int ACE_TMAIN(int argc, ACE_TCHAR **argv)
               }
 
               sent_tolpads.insert(std::string(tolpad.resource_id));
-              sent = true;
               break;
             }
           }
@@ -157,12 +157,11 @@ int ACE_TMAIN(int argc, ACE_TCHAR **argv)
       // Synchronize writer for route assignments
       OpenDDS::Model::WriterSync ws2(writer_route);
       {
-        bool sent = false;
-
         // Loop through requests to check for unprocessed ones
         for (const auto &auth : requests)
         {
-          if (sent_flight_ids.find(auth.flight_id) == sent_flight_ids.end() && auth.tolpad_id != "")
+          // if (sent_flight_ids.find(std::string(auth.flight_id)) == sent_flight_ids.end() && auth.tolpad_id != "")
+          if (sent_flight_ids.find(std::string(auth.flight_id)) == sent_flight_ids.end())
           {
             // Create a route assignment for the request
             UATM::acceptableRoute ar;
@@ -181,7 +180,50 @@ int ACE_TMAIN(int argc, ACE_TCHAR **argv)
                              ACE_TEXT(" write returned %d!\n"),
                          error));
             }
-            sent = true;
+
+            sent_flight_ids.insert(std::string(auth.flight_id.c_str()));
+            break;
+          }
+        }
+      }
+
+      OpenDDS::Model::WriterSync ws3(writer_auth);
+      {
+        for (const auto &auth : requests)
+        {
+          if (sent_flight_auth.find(auth.flight_id) == sent_flight_auth.end() && auth.tolpad_id != "")
+          {
+
+            std::string flowsID, restrictionID, weatherID;
+            bool flowOk = checkFlowConditions("uaspManagerDP/data/flows.txt", auth.departure_skyport_id, flowsID);
+            bool restrictionOk = checkRestrictionConditions("uaspManagerDP/data/restrictions.txt", auth.departure_skyport_id, restrictionID);
+            bool weatherOk = checkWeatherConditions("uaspManagerDP/data/weather.txt", auth.departure_skyport_id, weatherID);
+            UATM::flightAuthorization fa;
+            if (flowOk && restrictionOk && weatherOk)
+              fa.authorization_status = 1;
+            else
+              fa.authorization_status = 0;
+
+            fa.authorization_id = auth.request_id.c_str();
+            fa.flight_id = auth.flight_id.c_str();
+            fa.approved_route_id = "a";
+            fa.authorization_time = CORBA::string_dup(getCurrentTime().c_str());
+            fa.approved_departure_time = CORBA::string_dup(getCurrentTime().c_str());
+            fa.approved_arrival_time = CORBA::string_dup(getCurrentTime().c_str());
+            fa.tolpad_id = auth.tolpad_id.c_str();
+            fa.pilot_id = auth.pilot_id.c_str();
+            fa.evtol_id = auth.evtol_id.c_str();
+
+            DDS::ReturnCode_t error = writer_auth_var->write(fa, DDS::HANDLE_NIL);
+            if (error != DDS::RETCODE_OK)
+            {
+              ACE_ERROR((LM_ERROR,
+                         ACE_TEXT("(%P|%t) ERROR: %N:%l: main() -")
+                             ACE_TEXT(" write returned %d!\n"),
+                         error));
+            }
+
+            sent_flight_auth.insert(auth.flight_id);
             break;
           }
         }
@@ -190,17 +232,17 @@ int ACE_TMAIN(int argc, ACE_TCHAR **argv)
       // Synchronize writer for flight change records
       OpenDDS::Model::WriterSync ws4(writer_rec);
       {
-        bool sent = false;
-
         // Loop through requests to send flight change records
         for (const auto &auth : requests)
         {
-          if (sent_flight_ids.find(auth.flight_id) == sent_flight_ids.end() && auth.tolpad_id != "")
+          // if (sent_flight_recs.find(auth.flight_id) == sent_flight_recs.end())
+          if (i < 2)
           {
+            i++;
             UATM::flightChangeRec fc;
 
             fc.recommendation_id = recommendation_id++;
-            fc.flight_id = 1;
+            fc.flight_id = recommendation_id;
             fc.change_type = "change_type";
             fc.reason = "reason";
             fc.recommended_by = "recommended_by";
@@ -215,7 +257,7 @@ int ACE_TMAIN(int argc, ACE_TCHAR **argv)
                          error));
             }
 
-            sent = true;
+            sent_flight_recs.insert(std::to_string(fc.flight_id));
             break;
           }
         }
