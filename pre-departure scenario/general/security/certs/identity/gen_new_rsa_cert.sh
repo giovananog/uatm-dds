dd0e593ad12b3c8615749d001a56dc81745cdd6a
giovananog <giovana.nogueira@sou.unifal-mg.edu.br>
2024-12-27 16:04:10 -0300

    fix: update participants identity certs for security

--- pre-departure scenario/general/security/certs/identity/gen_new_rsa_cert.sh
+++ pre-departure scenario/general/security/certs/identity/gen_new_rsa_cert.sh
@@ -5,5 +5,5 @@ if [ $# -eq 0 ]
 fi
 
 openssl genrsa -out participants/$1_private_key.pem 2048
-openssl req -new -key participants/$1_private_key.pem -out participant/$1.csr
-openssl ca -config identity_ca_openssl.cnf -days 3650 -in participant/$1.csr -out participant/$1_cert.pem
+openssl req -new -key participants/$1_private_key.pem -out participants/$1.csr
+openssl ca -config identity_ca_openssl.cnf -days 3650 -in participants/$1.csr -out participants/$1_cert.pem
