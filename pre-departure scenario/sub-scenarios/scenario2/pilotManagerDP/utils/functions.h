8a7720dbeada3b617117f396fae2c496594ee95c
Giovana Nogueira <114829638+giovananog@users.noreply.github.com>
2024-12-27 16:07:59 -0300

    Delete pre-departure scenario/sub-scenarios directory

--- pre-departure scenario/sub-scenarios/scenario2/pilotManagerDP/utils/functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-#include <string>
-#include <vector>
-
-void updatePilotStatus(const std::string &pilot_id, int new_available, int new_sent);
-
-#endif // FUNCTIONS_H
